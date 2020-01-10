7954720aaee58e3409ca832452db9c485ecb903f
scottalmond <scottalmond13@gmail.com>
2020-01-10 15:24:27 -0800

    led blink test

---
+++ src/test/lcd/test_lcd/test_lcd/src/main.c
@@ -0,0 +1,79 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * This is a bare minimum user application template.
+ *
+ * For documentation of the board, go \ref group_common_boards "here" for a link
+ * to the board-specific documentation.
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# Minimal main function that starts with a call to system_init()
+ * -# Basic usage of on-board LED and button
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+
+#include <asf.h>
+
+/*#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+#include <hpl_gclk_base.h>
+#include <hpl_pm_base.h>
+#include <hal_delay.h>*/
+
+//#include <util/delay.h>
+
+//void digitalWrite(int pin_id,bool is_ON);
+
+int main (void)
+{
+	system_init();
+
+	/* Insert application code here, after the board has been initialized. */
+
+	while(1){
+		//digitalWrite(EXT1_PIN_3,true);
+		//delay_ms(1000);
+		//digitalWrite(EXT1_PIN_3,false);
+		//delay_ms(1000);
+		delay_ms(500); //cpu_delay_ms
+		port_pin_toggle_output_level(LED_0_PIN);
+	}
+
+	/* This skeleton code simply sets the LED to the state of the button. */
+	while (1) {
+		/* Is button pressed? */
+		if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
+			/* Yes, so turn LED on. */
+			port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		} else {
+			/* No, so turn LED off. */
+			port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		}
+	}
+}
+
+/*void digitalWrite(int pin_id,bool is_ON){
+	port_pin_set_output_level(pin_id,is_ON);
+}*/
